3ccd879d04937b8d8dc6de40e8fc11c96cd8395c
ruchirj <ruchir.jha@gmail.com>
2025-05-24 13:43:42 -0700

    Delay until after publications are configured

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,39 @@ repositories {
 }
 
 publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set("logsink")
+                description.set("A lightweight OTLP logs exporter for Java using OpenTelemetry.")
+                url.set("https://github.com/cardinalhq/logsink")
+
+                licenses {
+                    license {
+                        name.set("Apache-2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("ruchir")
+                        name.set("Ruchir Jha")
+                        email.set("ruchir@cardinalhq.io")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:https://github.com/cardinalhq/logsink.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:cardinalhq/logsink.git")
+                    url.set("https://github.com/cardinalhq/logsink")
+                }
+            }
+        }
+    }
+
     repositories {
         maven {
             name = "OSSRH"
@@ -35,6 +68,7 @@ signing {
         System.getenv("GPG_PASSPHRASE")
     )
 
+    // must be delayed until after `mavenJava` is created
     afterEvaluate {
         sign(publishing.publications["mavenJava"])
     }
@@ -45,16 +79,10 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
     testImplementation("org.mockito:mockito-core:5.11.0")
 
-    // SLF4J API (you log against this interface)
     implementation("org.slf4j:slf4j-api:2.0.13")
-
-    // Bridge: SLF4J -> Log4j2 (implementation of LoggerFactory)
     runtimeOnly("org.apache.logging.log4j:log4j-slf4j2-impl:2.22.1")
-
-    // Log4j2 core (does the actual logging)
     runtimeOnly("org.apache.logging.log4j:log4j-core:2.22.1")
 
-    // Jackson
     implementation("com.fasterxml.jackson.core:jackson-databind:2.19.0")
 
     implementation("io.opentelemetry:opentelemetry-sdk:1.49.0")
